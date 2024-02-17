f20460ed9e38f84129974ab5698e232843de6b52
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-02-17 08:31:57 -0300

    refactor: improves CharacterClassCreaterController handle method

--- src/presentation/controllers/CharacterClassCreater/index.ts
+++ src/presentation/controllers/CharacterClassCreater/index.ts
@@ -22,24 +22,21 @@ export class CharacterClassCreaterController implements Controller {
   async handle(
     params: CharacterClassCreaterControllerParams,
   ): Promise<HTTPResponse | HTTPErrorResponse> {
-    let response: HTTPResponse | HTTPErrorResponse
-
-    response = await this.validateDataAndResponseIfHasErrors(params.data)
-
-    if (response) {
-      return response
+    const validationResponse = await this.validateDataAndRespondIfHasErrors(
+      params.data,
+    )
+    if (validationResponse) {
+      return validationResponse
     }
 
     try {
-      response = await this.responseWithCreatedCharacterClass(params.data)
+      return await this.respondWithCreatedCharacterClass(params.data)
     } catch (error) {
-      response = this.responseWithHandledError(error)
-    } finally {
-      return response
+      return this.respondWithHandledError(error)
     }
   }
 
-  private async validateDataAndResponseIfHasErrors(
+  private async validateDataAndRespondIfHasErrors(
     data: CharacterClassCreaterParams,
   ): Promise<HTTPErrorResponse | undefined> {
     const { errors } = await this.dataValidator.validate(data)
@@ -52,7 +49,7 @@ export class CharacterClassCreaterController implements Controller {
     }
   }
 
-  private async responseWithCreatedCharacterClass(
+  private async respondWithCreatedCharacterClass(
     data: CharacterClassCreaterParams,
   ) {
     const createdCharacterClass = await this.characterClassCreater.create(data)
@@ -63,7 +60,7 @@ export class CharacterClassCreaterController implements Controller {
     }
   }
 
-  private responseWithHandledError(error: Error) {
+  private respondWithHandledError(error: Error) {
     return {
       statusCode: 500,
       errors: [error.message],

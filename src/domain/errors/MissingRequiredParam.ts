b87ca30e4111aa6a257d7031d7a988bbf0fdf32d
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-02-17 09:11:49 -0300

    refactor: deletes MissingRequiredParam error class

--- src/domain/errors/MissingRequiredParam.ts
+++
@@ -1,6 +0,0 @@
-export class MissingRequiredParam extends Error {
-  constructor(paramName: string) {
-    super(`Missing required param: ${paramName}`)
-    this.name = 'MissingRequiredParam'
-  }
-}

468b1f2fac6d1824fc532609442712953ed256cc
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-02-25 10:31:39 -0300

    refactor: improves ZodDataValidator.validate

--- src/infra/zod/DataValidator/index.ts
+++ src/infra/zod/DataValidator/index.ts
@@ -10,15 +10,23 @@ export class ZodDataValidator implements DataValidator {
 
   async validate(data: Record<string, any>): Promise<DataValidatorResult> {
     try {
-      await this.schema.parseAsync(data)
-      return { errors: [] }
+      return await this.handleValidation(data)
     } catch (error) {
-      if (error instanceof ZodError) {
-        const errors = adaptZodError(error)
-        return { errors }
-      } else {
-        throw error
-      }
+      return this.handleError(error)
+    }
+  }
+
+  private async handleValidation(data: Record<string, any>) {
+    await this.schema.parseAsync(data)
+    return { errors: [] }
+  }
+
+  private handleError(error: ZodError): DataValidatorResult {
+    if (error instanceof ZodError) {
+      const validationErrors = adaptZodError(error)
+      return { errors: validationErrors }
+    } else {
+      throw error
     }
   }
 }

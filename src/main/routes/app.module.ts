1476dedc62b7f4ae06a281e6cb40c4609497fc31
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-02-26 09:14:38 -0300

    refactor: removes unused code

--- src/main/routes/app.module.ts
+++ src/main/routes/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { ClassesModule } from './classes/classes.module';
+import { ClassesModule } from './classes/classes.module'
 
 @Module({
   imports: [ClassesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

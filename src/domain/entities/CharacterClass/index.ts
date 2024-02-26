ac89c938bad6b283a457069cdeaee08158cb87f9
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-02-26 18:10:41 -0300

    feat: adds subclasses to CharacterClass interface and mock

--- src/domain/entities/CharacterClass/index.ts
+++ src/domain/entities/CharacterClass/index.ts
@@ -1,4 +1,7 @@
+import { Subclass } from '../Subclass'
+
 export interface CharacterClass {
   id: string
   name: string
+  subclasses: Subclass[]
 }
--- src/domain/entities/CharacterClass/mock.ts
+++ src/domain/entities/CharacterClass/mock.ts
@@ -1,7 +1,9 @@
 import { CharacterClass } from '.'
 import { faker } from '@faker-js/faker'
+import { mockSubclass } from '../Subclass/mock'
 
 export const mockCharacterClass = (): CharacterClass => ({
   id: faker.string.uuid(),
   name: faker.lorem.word(),
+  subclasses: [mockSubclass(), mockSubclass(), mockSubclass()],
 })

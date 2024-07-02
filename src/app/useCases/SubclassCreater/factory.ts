64ab5534b2757e885f61e1f45733e0d7f796adc1
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-07-02 06:51:03 -0300

    feat: adds factory function for SubclassCreater use case

---
+++ src/app/useCases/SubclassCreater/factory.ts
@@ -0,0 +1,12 @@
+import { PrismaSubclassCreaterRepo } from 'infra/prisma/repositories/SubclassCreaterRepo'
+import { SubclassCreater } from '.'
+import { PinoErrorLoggerRepo } from 'infra/pino/repositories/ErrorLoggerRepository'
+import { pinoLogger } from 'infra/pino'
+
+export const makeSubclassCreater = (): SubclassCreater => {
+  const subclassCreaterRepo = new PrismaSubclassCreaterRepo()
+  const errorLoggerRepo = new PinoErrorLoggerRepo(pinoLogger)
+  const useCase = new SubclassCreater(subclassCreaterRepo, errorLoggerRepo)
+
+  return useCase
+}

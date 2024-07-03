6b9050a3e5868b4b19825f013f4bcef44505f915
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-07-03 06:57:45 -0300

    feat: adds Prisma seed script and DBSeeder class for seeding the database

---
+++ src/infra/prisma/seeds/DBSeeder.ts
@@ -0,0 +1,44 @@
+import { mockPrismaSubclass } from '../data/Subclass/mock'
+import { mockPrismaClass } from '../data/Class/mock'
+import { PrismaClass } from '../data/Class'
+import { PrismaSubclass } from '../data/Subclass'
+import prisma from '..'
+
+type OptionalProp<T, K extends keyof T> = Omit<T, K> & {
+  [P in K]?: T[P]
+}
+
+export class DBSeeder {
+  async seed() {
+    const classCreated = await this.seedClass()
+    const subclassCreated = await this.seedSubclass(classCreated.id)
+
+    return {
+      classCreated,
+      subclassCreated,
+    }
+  }
+
+  private async seedClass() {
+    const classToCreate = mockPrismaClass() as OptionalProp<PrismaClass, 'id'>
+    delete classToCreate.id
+    const classCreated = await prisma.class.create({
+      data: classToCreate,
+    })
+
+    return classCreated
+  }
+
+  private async seedSubclass(classID: number) {
+    const subclassToCreate = mockPrismaSubclass() as OptionalProp<
+      PrismaSubclass,
+      'id'
+    >
+    delete subclassToCreate.id
+    const subclassCreated = await prisma.subclass.create({
+      data: { ...subclassToCreate, classID },
+    })
+
+    return subclassCreated
+  }
+}
---
+++ src/infra/prisma/seeds/index.ts
@@ -0,0 +1,27 @@
+import prisma from '..'
+import { DBSeeder } from './DBSeeder'
+
+async function main() {
+  console.log('Start seeding...')
+  const dbSeeder = new DBSeeder()
+
+  for (let i = 0; i < 10; i++) {
+    const { classCreated, subclassCreated } = await dbSeeder.seed()
+    console.log(`Seeding iteration ${i + 1} finished:`, {
+      classCreated,
+      subclassCreated,
+    })
+  }
+
+  console.log('All seeding operations completed.')
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })

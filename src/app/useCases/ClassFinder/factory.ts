de31c22174d332b82f25ce6375d980943a58df4d
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-07-16 18:33:28 -0300

    feat: adds factory function for ClassFinder use case

---
+++ src/app/useCases/ClassFinder/factory.ts
@@ -0,0 +1,12 @@
+import { PrismaClassFinderRepo } from 'infra/prisma/repositories/ClassFinderRepo'
+import { ClassFinder } from '.'
+import { PinoErrorLoggerRepo } from 'infra/pino/repositories/ErrorLoggerRepository'
+import { pinoLogger } from 'infra/pino'
+
+export const makeClassFinder = (): ClassFinder => {
+  const characterClassFinderRepo = new PrismaClassFinderRepo()
+  const errorLoggerRepo = new PinoErrorLoggerRepo(pinoLogger)
+  const useCase = new ClassFinder(characterClassFinderRepo, errorLoggerRepo)
+
+  return useCase
+}

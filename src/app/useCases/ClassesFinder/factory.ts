b7fcb40321b0a3311a63625af800c8abf22457cc
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-07-19 17:43:22 -0300

    refactor: renames makeClassCreater to makeClassesCreater in factory.ts

--- src/app/useCases/ClassesFinder/factory.ts
+++ src/app/useCases/ClassesFinder/factory.ts
@@ -3,10 +3,10 @@ import { PinoErrorLoggerRepo } from 'infra/pino/repositories/ErrorLoggerReposito
 import { pinoLogger } from 'infra/pino'
 import { PrismaClassesFinderRepo } from 'infra/prisma/repositories/ClassesFinderRepo'
 
-export const makeClassCreater = (): ClassesFinder => {
-  const classCreaterRepo = new PrismaClassesFinderRepo()
+export const makeClassesCreater = (): ClassesFinder => {
+  const classesCreaterRepo = new PrismaClassesFinderRepo()
   const errorLoggerRepo = new PinoErrorLoggerRepo(pinoLogger)
-  const useCase = new ClassesFinder(classCreaterRepo, errorLoggerRepo)
+  const useCase = new ClassesFinder(classesCreaterRepo, errorLoggerRepo)
 
   return useCase
 }

2d1ba366ff924d126f80ebeff370bc6bfccc2e8d
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-07-19 17:37:43 -0300

    feat: adds PrismaClassesFinderRepo

---
+++ src/infra/prisma/repositories/ClassesFinderRepo/index.ts
@@ -0,0 +1,25 @@
+import {
+  ClassesFinderRepo,
+  ClassesFinderRepoParams,
+} from 'app/interfaces/ClassesFinderRepo'
+import { Class } from 'domain/entities/Class'
+import prisma from 'infra/prisma'
+import { adaptPrismaClass } from 'infra/prisma/adapters/adaptPrismaClass'
+
+export class PrismaClassesFinderRepo implements ClassesFinderRepo {
+  async find(params: ClassesFinderRepoParams): Promise<Class[]> {
+    const { filter, include } = params
+    const prismaClasses = await prisma.class.findMany({
+      where: {
+        name: {
+          contains: filter.name?.like,
+        },
+      },
+      include: {
+        subclasses: include?.subclasses,
+      },
+    })
+
+    return prismaClasses.map((prismaClass) => adaptPrismaClass(prismaClass))
+  }
+}
---
+++ src/infra/prisma/repositories/ClassesFinderRepo/test.ts
@@ -0,0 +1,49 @@
+import { Prisma } from '@prisma/client'
+import { PrismaClassesFinderRepo } from '.'
+import { DefaultArgs } from '@prisma/client/runtime/library'
+import { mockedPrismaClient } from 'infra/prisma/mock'
+import { mockPrismaClass } from 'infra/prisma/data/Class/mock'
+import { adaptPrismaClass } from 'infra/prisma/adapters/adaptPrismaClass'
+import { mockClassesFinderRepoParams } from 'app/interfaces/ClassesFinderRepo/mock'
+
+const makeSUT = () => {
+  mockedPrismaClient.class.findMany.mockResolvedValue([mockPrismaClass()])
+
+  const sut = new PrismaClassesFinderRepo()
+  return { sut }
+}
+
+describe('PrismaClassesFinderRepo', () => {
+  it('should call prisma client with right params', async () => {
+    const { sut } = makeSUT()
+
+    const params = mockClassesFinderRepoParams()
+    await sut.find(params)
+
+    const { filter, include } = params
+    const expectedParams: Prisma.ClassFindManyArgs<DefaultArgs> = {
+      where: {
+        name: {
+          contains: filter.name?.like,
+        },
+      },
+      include,
+    }
+    expect(mockedPrismaClient.class.findMany).toHaveBeenCalledWith(
+      expectedParams,
+    )
+  })
+
+  it('should find classes', async () => {
+    const { sut } = makeSUT()
+
+    const params = mockClassesFinderRepoParams()
+    const prismaClass = mockPrismaClass()
+    mockedPrismaClient.class.findMany.mockResolvedValue([prismaClass])
+
+    const result = await sut.find(params)
+
+    const expectedClass = adaptPrismaClass(prismaClass)
+    expect(result).toEqual(expectedClass)
+  })
+})

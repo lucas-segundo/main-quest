39e0f1a68384964510860fe35e33ea0639619052
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-07-29 17:56:01 -0300

    feat: adds factory function for ClassUpdater use case

---
+++ src/app/useCases/ClassUpdater/factory.ts
@@ -0,0 +1,12 @@
+import { PrismaClassUpdaterRepo } from 'infra/prisma/repositories/ClassUpdaterRepo'
+import { ClassUpdater } from '.'
+import { PinoErrorLoggerRepo } from 'infra/pino/repositories/ErrorLoggerRepository'
+import { pinoLogger } from 'infra/pino'
+
+export const makeClassUpdater = (): ClassUpdater => {
+  const classUpdaterRepo = new PrismaClassUpdaterRepo()
+  const errorLoggerRepo = new PinoErrorLoggerRepo(pinoLogger)
+  const useCase = new ClassUpdater(classUpdaterRepo, errorLoggerRepo)
+
+  return useCase
+}

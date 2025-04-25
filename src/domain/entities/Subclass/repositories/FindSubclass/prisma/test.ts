1eeb1e7596dbf9f8e95c7f6ea5f23820cfef8177
Lucas Segundo <lucas.fernandes.app@gmail.com>
2025-04-25 06:39:06 -0300

    refactor: update tests in PrismaFindSubclass to correctly handle subclass logic

--- src/domain/entities/Subclass/repositories/FindSubclass/prisma/test.ts
+++ src/domain/entities/Subclass/repositories/FindSubclass/prisma/test.ts
@@ -2,12 +2,11 @@ import { Prisma } from '@prisma/client'
 import { PrismaFindSubclass } from '.'
 import { DefaultArgs } from '@prisma/client/runtime/library'
 import { mockedPrismaClient } from 'infra/prisma/mock'
-import { mockPrismaClass } from 'infra/prisma/data/Class/mock'
-import { adaptPrismaClass } from 'infra/prisma/adapters/adaptPrismaClass'
 import { NotFoundError } from 'domain/errors/NotFoundError'
 import { mockPrismaSubclass } from 'infra/prisma/data/Subclass/mock'
 import { mockFindClassRepositoryParams } from 'domain/entities/Class/repositories/FindClass/mock'
 import { mockFindSubclassRepositoryParams } from '../mock'
+import { adaptPrismaSubclass } from 'infra/prisma/adapters/adaptPrismaSubclass'
 
 const makeSUT = () => {
   mockedPrismaClient.subclass.findFirst.mockResolvedValue(mockPrismaSubclass())
@@ -29,28 +28,28 @@ describe('PrismaFindSubclass', () => {
         id: Number(filter.id.equals),
       },
     }
-    expect(mockedPrismaClient.class.findFirst).toHaveBeenCalledWith(
+    expect(mockedPrismaClient.subclass.findFirst).toHaveBeenCalledWith(
       expectedParams,
     )
   })
 
-  it('should find a class', async () => {
+  it('should find a subclass', async () => {
     const { sut } = makeSUT()
 
     const params = mockFindClassRepositoryParams()
-    const prismaClass = mockPrismaClass()
-    mockedPrismaClient.class.findFirst.mockResolvedValue(prismaClass)
+    const prismaSubclass = mockPrismaSubclass()
+    mockedPrismaClient.subclass.findFirst.mockResolvedValue(prismaSubclass)
 
     const result = await sut.find(params)
 
-    const expectedClass = adaptPrismaClass(prismaClass)
+    const expectedClass = adaptPrismaSubclass(prismaSubclass)
     expect(result).toEqual(expectedClass)
   })
 
-  it('should throw if class not found', async () => {
+  it('should throw if subclass not found', async () => {
     const { sut } = makeSUT()
 
-    mockedPrismaClient.class.findFirst.mockResolvedValue(null)
+    mockedPrismaClient.subclass.findFirst.mockResolvedValue(null)
 
     const params = mockFindClassRepositoryParams()
     await expect(sut.find(params)).rejects.toThrow(NotFoundError)

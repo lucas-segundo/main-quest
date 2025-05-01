d70118871cea58e23b3e9a4e7f999173da70b39f
Lucas Segundo <lucas.fernandes.app@gmail.com>
2025-05-01 09:13:39 -0300

    refactor: update Controller interface to allow variable number of parameters in handle method

--- src/presentation/interfaces/Controller/index.ts
+++ src/presentation/interfaces/Controller/index.ts
@@ -14,5 +14,5 @@ export interface HTTPResponse {
 }
 
 export interface Controller {
-  handle: (params: any) => Promise<HTTPResponse | HTTPErrorResponse>
+  handle: (...params: any) => Promise<HTTPResponse | HTTPErrorResponse>
 }

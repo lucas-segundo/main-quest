f5cbc4e99af9f8b09bc3aae70e9c45515d862eeb
Lucas Segundo <lucas.fernandes.app@gmail.com>
2025-05-03 12:00:15 -0300

    refactor: remove unused skill controllers and related imports from SkillsController and SkillsModule

--- src/main/routes/skills/skills.controller.ts
+++ src/main/routes/skills/skills.controller.ts
@@ -1,52 +1,19 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Patch,
-  Post,
-  Query,
-  Res,
-} from '@nestjs/common'
+import { Controller, Get, Param, Query, Res } from '@nestjs/common'
 import { Response } from 'express'
 import { FindSkillController } from 'presentation/controllers/skills/FindSkill'
-import {
-  UpdateSkillController,
-  UpdateSkillControllerParams,
-} from 'presentation/controllers/skills/UpdateSkill'
+import { UpdateSkillController } from 'presentation/controllers/skills/UpdateSkill'
 import {
   FindSkillsController,
   FindSkillsControllerParams,
 } from 'presentation/controllers/skills/FindSkills'
-import {
-  CreateSkillController,
-  CreateSkillControllerParams,
-} from 'presentation/controllers/skills/CreateSkill'
 
 @Controller('skills')
 export class SkillsController {
   constructor(
-    private readonly createSkillController: CreateSkillController,
     private readonly findSkillController: FindSkillController,
     private readonly findSkillsController: FindSkillsController,
     private readonly updateSkillController: UpdateSkillController,
   ) {}
-
-  @Post()
-  async create(
-    @Body() body: CreateSkillControllerParams,
-    @Res() res: Response,
-  ) {
-    const response = await this.createSkillController.handle(body)
-
-    if ('data' in response) {
-      const { data, statusCode } = response
-      return res.status(statusCode).json({ data })
-    } else {
-      const { errors, statusCode } = response
-      return res.status(statusCode).json({ errors })
-    }
-  }
 
   @Get(':id')
   async find(@Param('id') id: string, @Res() res: Response) {
@@ -74,26 +41,6 @@ export class SkillsController {
   ) {
     const response = await this.findSkillsController.handle({
       filter: query.filter,
-    })
-
-    if ('data' in response) {
-      const { data, statusCode } = response
-      return res.status(statusCode).json({ data })
-    } else {
-      const { errors, statusCode } = response
-      return res.status(statusCode).json({ errors })
-    }
-  }
-
-  @Patch(':id')
-  async update(
-    @Param('id') id: string,
-    @Body() data: UpdateSkillControllerParams['data'],
-    @Res() res: Response,
-  ) {
-    const response = await this.updateSkillController.handle({
-      id,
-      data,
     })
 
     if ('data' in response) {
--- src/main/routes/skills/skills.module.ts
+++ src/main/routes/skills/skills.module.ts
@@ -1,21 +1,12 @@
 import { Module } from '@nestjs/common'
 import { SkillsController } from './skills.controller'
-import { CreateSkillController } from 'presentation/controllers/skills/CreateSkill'
-import { makeCreateSkillController } from 'presentation/controllers/skills/CreateSkill/factory'
 import { FindSkillController } from 'presentation/controllers/skills/FindSkill'
 import { makeFindSkillController } from 'presentation/controllers/skills/FindSkill/factory'
 import { FindSkillsController } from 'presentation/controllers/skills/FindSkills'
 import { makeFindSkillsController } from 'presentation/controllers/skills/FindSkills/factory'
-import { makeUpdateSkillController } from 'presentation/controllers/skills/UpdateSkill/factory'
-import { UpdateSkillController } from 'presentation/controllers/skills/UpdateSkill'
-
 @Module({
   controllers: [SkillsController],
   providers: [
-    {
-      provide: CreateSkillController,
-      useFactory: () => makeCreateSkillController(),
-    },
     {
       provide: FindSkillController,
       useFactory: () => makeFindSkillController(),
@@ -23,10 +14,6 @@ import { UpdateSkillController } from 'presentation/controllers/skills/UpdateSki
     {
       provide: FindSkillsController,
       useFactory: () => makeFindSkillsController(),
-    },
-    {
-      provide: UpdateSkillController,
-      useFactory: () => makeUpdateSkillController(),
     },
   ],
 })

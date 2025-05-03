9c8714973be0941787cfadbb3269829bc4f25f04
Lucas Segundo <lucas.fernandes.app@gmail.com>
2025-05-03 09:03:46 -0300

    chore: create initial migration for classes, subclasses, skills, and characters tables with foreign key constraints

--- src/infra/prisma/migrations/20240219114030_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "classes" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "classes_pkey" PRIMARY KEY ("id")
-);
--- src/infra/prisma/migrations/20250503120311_init/migration.sql
+++ src/infra/prisma/migrations/20250503120311_init/migration.sql
@@ -1,4 +1,21 @@
 -- CreateTable
+CREATE TABLE "classes" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "classes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "subclasses" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "class_id" INTEGER NOT NULL,
+
+    CONSTRAINT "subclasses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "skills" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
@@ -24,6 +41,19 @@ CREATE TABLE "subclasses_skills" (
     CONSTRAINT "subclasses_skills_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "characters" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "class_id" INTEGER NOT NULL,
+    "level" INTEGER NOT NULL,
+
+    CONSTRAINT "characters_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "subclasses" ADD CONSTRAINT "subclasses_class_id_fkey" FOREIGN KEY ("class_id") REFERENCES "classes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
 -- AddForeignKey
 ALTER TABLE "classes_skills" ADD CONSTRAINT "classes_skills_class_id_fkey" FOREIGN KEY ("class_id") REFERENCES "classes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -35,3 +65,6 @@ ALTER TABLE "subclasses_skills" ADD CONSTRAINT "subclasses_skills_subclass_id_fk
 
 -- AddForeignKey
 ALTER TABLE "subclasses_skills" ADD CONSTRAINT "subclasses_skills_skill_id_fkey" FOREIGN KEY ("skill_id") REFERENCES "skills"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "characters" ADD CONSTRAINT "characters_class_id_fkey" FOREIGN KEY ("class_id") REFERENCES "classes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

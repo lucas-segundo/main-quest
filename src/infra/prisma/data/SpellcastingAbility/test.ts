1c1cc1b850d1703a32b027fd4c4601fe13d96dc7
Lucas Segundo <lucas.fernandes.app@gmail.com>
2025-05-08 18:10:01 -0300

    feat: add SpellcastingAbility adaptation functions and corresponding tests

---
+++ src/infra/prisma/data/SpellcastingAbility/test.ts
@@ -0,0 +1,35 @@
+import { adaptSpellcastingAbility, adaptToPrismaSpellcastingAbility } from '.'
+
+describe('adaptSpellcastingAbility', () => {
+  it('should correctly map "CHA" to "charisma"', () => {
+    const result = adaptSpellcastingAbility('CHA')
+    expect(result).toBe('charisma')
+  })
+
+  it('should correctly map "WIS" to "wisdom"', () => {
+    const result = adaptSpellcastingAbility('WIS')
+    expect(result).toBe('wisdom')
+  })
+
+  it('should correctly map "INT" to "intelligence"', () => {
+    const result = adaptSpellcastingAbility('INT')
+    expect(result).toBe('intelligence')
+  })
+})
+
+describe('adaptToPrismaSpellcastingAbility', () => {
+  it('should correctly map "charisma" to "CHA"', () => {
+    const result = adaptToPrismaSpellcastingAbility('charisma')
+    expect(result).toBe('CHA')
+  })
+
+  it('should correctly map "wisdom" to "WIS"', () => {
+    const result = adaptToPrismaSpellcastingAbility('wisdom')
+    expect(result).toBe('WIS')
+  })
+
+  it('should correctly map "intelligence" to "INT"', () => {
+    const result = adaptToPrismaSpellcastingAbility('intelligence')
+    expect(result).toBe('INT')
+  })
+})

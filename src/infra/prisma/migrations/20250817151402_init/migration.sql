27b4cfdbeb86e01dc14f903bc6b868e1727cb410
Lucas Segundo <lucas.fernandes.app@gmail.com>
2025-08-17 12:14:27 -0300

    feat: add initial database migration with tables for classes, subclasses, spells, and characters

---
+++ src/infra/prisma/migrations/20250817151402_init/migration.sql
@@ -0,0 +1,89 @@
+-- CreateEnum
+CREATE TYPE "SpellcastingAbility" AS ENUM ('INT', 'WIS', 'CHA');
+
+-- CreateTable
+CREATE TABLE "classes" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "hit_dice" TEXT NOT NULL,
+    "spellcasting_ability" "SpellcastingAbility",
+
+    CONSTRAINT "classes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "subclasses" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "class_id" INTEGER NOT NULL,
+
+    CONSTRAINT "subclasses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "spells" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "spells_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "classes_spells" (
+    "id" SERIAL NOT NULL,
+    "class_id" INTEGER NOT NULL,
+    "spell_id" INTEGER NOT NULL,
+
+    CONSTRAINT "classes_spells_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "subclasses_spells" (
+    "id" SERIAL NOT NULL,
+    "subclass_id" INTEGER NOT NULL,
+    "spell_id" INTEGER NOT NULL,
+
+    CONSTRAINT "subclasses_spells_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "characters" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "class_id" INTEGER NOT NULL,
+    "level" INTEGER NOT NULL,
+    "hit_points" INTEGER NOT NULL,
+    "max_hit_points" INTEGER NOT NULL,
+    "strength" INTEGER NOT NULL,
+    "dexterity" INTEGER NOT NULL,
+    "constitution" INTEGER NOT NULL,
+    "intelligence" INTEGER NOT NULL,
+    "wisdom" INTEGER NOT NULL,
+    "charisma" INTEGER NOT NULL,
+
+    CONSTRAINT "characters_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "characters_name_key" ON "characters"("name");
+
+-- CreateIndex
+CREATE INDEX "characters_name_idx" ON "characters"("name");
+
+-- AddForeignKey
+ALTER TABLE "subclasses" ADD CONSTRAINT "subclasses_class_id_fkey" FOREIGN KEY ("class_id") REFERENCES "classes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "classes_spells" ADD CONSTRAINT "classes_spells_class_id_fkey" FOREIGN KEY ("class_id") REFERENCES "classes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "classes_spells" ADD CONSTRAINT "classes_spells_spell_id_fkey" FOREIGN KEY ("spell_id") REFERENCES "spells"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subclasses_spells" ADD CONSTRAINT "subclasses_spells_subclass_id_fkey" FOREIGN KEY ("subclass_id") REFERENCES "subclasses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subclasses_spells" ADD CONSTRAINT "subclasses_spells_spell_id_fkey" FOREIGN KEY ("spell_id") REFERENCES "spells"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "characters" ADD CONSTRAINT "characters_class_id_fkey" FOREIGN KEY ("class_id") REFERENCES "classes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
